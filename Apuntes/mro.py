a3e2c98de79081734a918a24f2309edef25ed327
Alejandro Barraza <abarraza.pk@gmail.com>
2023-12-04 11:02:02 -0300

    Apuntes de MRO listos.

---
+++ Apuntes/mro.py
@@ -0,0 +1,32 @@
+class F:
+    def hablar(self):
+        print("hola desde F")
+
+
+class A:
+    def hablar(self):
+        print("hola desde A")
+
+
+class B(A):
+    def hablar(self):
+        print("hola desde B")
+
+
+class C(A):
+    def hablar(self):
+        print("hola desde C")
+
+
+class D(B, C):
+    pass
+
+
+d = D()
+
+d.hablar()
+
+# Forzar que un objeto utilize un método de la clase A, pasándole el objeto como parámetro self
+# A.hablar(d)
+
+print(D.mro())
