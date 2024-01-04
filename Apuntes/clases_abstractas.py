1a4b550b9b3a768ed05126aa80b8790f1c146131
Alejandro Barraza <abarraza.pk@gmail.com>
2024-01-04 14:56:35 -0300

    Apuntes de clases abstractas listos.

---
+++ Apuntes/clases_abstractas.py
@@ -0,0 +1,43 @@
+from abc import ABC, abstractclassmethod
+
+
+class Persona(ABC):
+    @abstractclassmethod
+    def __init__(self, nombre, apellido, edad, actividad):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.edad = edad
+        self.actividad = actividad
+
+    @abstractclassmethod
+    def hacer_actividad(self):
+        pass
+
+    def presentarse(self):
+        print(f"Hola me llamo: {self.nombre} {self.apellido}. Tengo {self.edad} años")
+
+
+class Estudiante(Persona):
+    def __init__(self, nombre, apellido, edad, actividad):
+        super().__init__(nombre, apellido, edad, actividad)
+
+    def hacer_actividad(self):
+        print(f"Estoy estudiando: {self.actividad}")
+
+
+class Trabajador(Persona):
+    def __init__(self, nombre, apellido, edad, actividad):
+        super().__init__(nombre, apellido, edad, actividad)
+
+    def hacer_actividad(self):
+        print(f"Estoy trabajando en el rubro de: {self.actividad}")
+
+
+# Ya que Persona es una clase abstracta, no se puede instanciar.
+# Ale = Persona("Alejandro", "Barraza", 8)
+
+Ale = Estudiante("Alejandro", "Barraza", 18, "programación")
+Roberto = Trabajador("Roberto", "Barraza", 25, "desarrollo de software")
+
+Ale.hacer_actividad()
+Roberto.hacer_actividad()
